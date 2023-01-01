9005778f5b2613532a518b78a9e197b86575d99b
Aelso <49383092+Aelso@users.noreply.github.com>
2023-01-01 18:28:14 -0300

    primeiro commit

---
+++ assets/js/canva.js
@@ -0,0 +1,72 @@
+function drawCanvas() {
+    tabuleiro.lineWidth = 8;
+    tabuleiro.lineCap = "round";
+    tabuleiro.lineJoin = "round";
+    tabuleiro.fillStyle = "#F3F5FC";
+    tabuleiro.strokeStyle = "#D64D00";
+
+    //manipulação
+    tabuleiro.fillRect(0, 0, 1200, 800);
+    tabuleiro.beginPath();
+    tabuleiro.moveTo(450, 450);
+    tabuleiro.lineTo(750, 450);
+    tabuleiro.stroke();
+    tabuleiro.closePath();
+}
+
+function drawLines() {
+    tabuleiro.lineWidth = 6;
+    tabuleiro.lineCap = "round";
+    tabuleiro.lineJoin = "round";
+    tabuleiro.fillStyle = "#D64D00";
+    tabuleiro.strokeStyle = "#D64D00";
+
+    let width = 600/secretWord.length
+    for (let i = 0; i < secretWord.length; i++) {
+        tabuleiro.moveTo(320 + (width * i), 640);
+        tabuleiro.lineTo(380 + (width * i), 640);
+    }
+    tabuleiro.stroke();
+    tabuleiro.closePath();
+}
+
+//Escrever a letra correta
+function writeCorrectLetter(index) {
+    tabuleiro.font = "bold 52px Inter";
+    tabuleiro.lineCap = "round";
+    tabuleiro.fillStyle = "#D64D00";
+    
+    tabuleiro.lineWidth = 6;
+    let width = 600/secretWord.length;
+    tabuleiro.fillText(secretWord[index], 330 + (width * index), 630);
+}
+
+//Escrever a letra incorreta
+function writeIncorrectLetter(letter, erros) {
+    tabuleiro.font = "bold 40px Inter";
+    tabuleiro.lineCap = "round";
+    tabuleiro.fillStyle = "#D64D00";
+    
+    tabuleiro.lineWidth = 4;
+    tabuleiro.fillText(letter, 360 + (40 * (10 - erros)), 700, 40)
+
+    drawHangMan(erros);
+}
+
+function endGame() {
+    tabuleiro.font = "bold 52px";
+    tabuleiro.lineCap = "round";
+    tabuleiro.fillStyle = "#CA2501";
+    
+    tabuleiro.lineWidth = 6;
+    tabuleiro.fillText("Você perdeu!", 750, 330)
+}
+
+function winGame() {
+    tabuleiro.font = "bold 52px";
+    tabuleiro.lineCap = "round";
+    tabuleiro.fillStyle = "#47AF48";
+    
+    tabuleiro.lineWidth = 6;
+    tabuleiro.fillText("Você ganhou!", 750, 330)
+}
---
+++ assets/js/script.js
@@ -0,0 +1,125 @@
+let tabuleiro = document.getElementById('hangman').getContext('2d');
+let words = ["LOGICA", "HTML", "CSS", "FORCA", "ESTUDO", "DESAFIO"];
+let secretWord = "";
+let erros = 8;
+let letters = [];
+let yesLetters = [];
+let sortedYesLetters;
+let sortedWordLetters;
+
+function chooseSecretWord() {
+    let word = words[Math.floor(Math.random() * words.length)];
+    secretWord = word;
+//    console.log(secretWord);
+}
+
+function addWord() {
+    document.querySelector("#menu").style.display = "none";
+    document.querySelector("#new-word").style.display = "block";
+}
+
+function cancelAddWord() {
+    document.querySelector("#new-word").style.display = "none";
+    document.querySelector("#menu").style.display = "block";
+}
+
+function giveUpGame() {
+    document.querySelector("#start-game").style.display = "none";
+    document.querySelector("#menu").style.display = "block";
+}
+
+//Verificar se tecla digitada é uma letra
+function checkLetter(key) {
+    let status = false;
+    if (key >= 65 && letters.indexOf(key) || key <= 90 && letters.indexOf(key)) {
+        letters.push(key);
+        return status
+    } else {
+        status = true;
+        letters.push(key);
+        return status
+    }
+}
+
+function countErrors() {
+    erros -= 1; 
+}
+ 
+function startGame() {
+    document.querySelector("#menu").style.display = "none";
+    document.querySelector("#start-game").style.display = "block";
+    document.querySelector("#new-word").style.display = "none";
+    chooseSecretWord();
+
+    erros = 8;
+    yesLetters = [];
+
+    wordLetters();
+    drawCanvas();
+    drawLines();
+    drawHangMan();
+    
+    //Captar a letra digitada
+    
+   do {
+    document.onkeydown = (e) => {
+        if (erros > 0) {
+            let letter = e.key.toUpperCase();
+
+            if (checkLetter(letter) && secretWord.includes(letter)) {                
+            //Saber se a palavra secreta inclui a letra digitada
+                for (let i = 0; i < secretWord.length; i++) {
+                    if (secretWord[i] === letter) {
+                        writeCorrectLetter(i);
+
+                        if (yesLetters.length != secretWord.length && yesLetters.includes(letter) == false) {
+                            yesLetters.push(letter);
+                        }                                           
+                    }
+                }
+                checkWin();
+            } else {
+                countErrors(letter);      
+                writeIncorrectLetter(letter, erros);               
+            }
+        } 
+
+    }
+    } while (sortedWordLetters != sortedYesLetters);
+
+}
+   
+function addNewWord() {
+    newWord = document.getElementById("inputNewWord").value.toUpperCase();
+    words.push(newWord);
+    document.getElementById("inputNewWord").value = "";
+
+    //CONDIONAR PALAVRA <= 8 LETRAS
+}
+
+function wordLetters() {
+    lettersInTheWord = [];
+
+    for (x = 0; x <= secretWord.length; x++) {
+        if (lettersInTheWord.includes(secretWord[x]) == false)  {
+            lettersInTheWord.push(secretWord[x]);
+        }
+    }
+    lettersInTheWord.pop();
+}
+
+function checkWin() {
+  //SORT
+  lettersInTheWord.sort();
+  yesLetters.sort(); 
+
+  //JOIN
+  sortedWordLetters = lettersInTheWord.join("");
+  sortedYesLetters = yesLetters.join("");
+
+  //COMPARE
+  if (sortedWordLetters === sortedYesLetters) {
+      winGame();
+  }
+}
+
